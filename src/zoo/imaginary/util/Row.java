db0f86e4f3066aa4f98ba039237255c69aae0b78
Peti <antalpeti@users.noreply.github.com>
2015-05-25 08:36:12 +0200

    Added method to find all the header of columns. Remove unused method. Refactor.

--- src/zoo/imaginary/util/Row.java
+++ src/zoo/imaginary/util/Row.java
@@ -8,15 +8,11 @@ public class Row {
   private List<Column> columns;
 
   public Row() {
-    setColumns(new ArrayList<Column>());
+    columns = new ArrayList<Column>();
   }
 
   public List<Column> getColumns() {
     return columns;
-  }
-
-  public void setColumns(List<Column> columns) {
-    this.columns = columns;
   }
 
   @Override
--- src/zoo/imaginary/util/XmlUtils.java
+++ src/zoo/imaginary/util/XmlUtils.java
@@ -1,19 +1,13 @@
 package zoo.imaginary.util;
 
 import java.io.File;
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.SAXParser;
 import javax.xml.parsers.SAXParserFactory;
-
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-
-
 
 public class XmlUtils {
 
@@ -22,7 +16,8 @@ public class XmlUtils {
     for (Row row : rows) {
       System.out.println(row);
     }
-    // parseXmlFileByDom("database.xml");
+    String[] columns = findAllColumns(rows);
+    System.out.println(Arrays.toString(columns));
   }
 
   public static List<Row> parseXmlFileBySax(String filePath) {
@@ -37,76 +32,6 @@ public class XmlUtils {
       e.printStackTrace();
     }
     return userhandler.getRows();
-  }
-
-  public static void parseXmlFileByDom(String filePath) {
-    try {
-      File inputFile = new File(filePath);
-      DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-      DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-      Document doc = dBuilder.parse(inputFile);
-      doc.getDocumentElement().normalize();
-
-      System.out.print("Root element: ");
-      System.out.println(doc.getDocumentElement().getNodeName());
-      // upgroup
-      NodeList upgroupList = doc.getElementsByTagName("upgroup");
-      for (int upgroupIndex = 0; upgroupIndex < upgroupList.getLength(); upgroupIndex++) {
-        Node upgroupNode = upgroupList.item(upgroupIndex);
-        System.out.print(upgroupNode.getNodeName());
-        if (upgroupNode.getNodeType() == Node.ELEMENT_NODE) {
-          Element upgroupElement = (Element) upgroupNode;
-          System.out.print(" name : ");
-          System.out.println(upgroupElement.getAttribute("name"));
-          // midgroup
-          NodeList midgroupList = doc.getElementsByTagName("midgroup");
-          for (int midgroupIndex = 0; midgroupIndex < midgroupList.getLength(); midgroupIndex++) {
-            Node midgroupNode = midgroupList.item(midgroupIndex);
-            System.out.print(midgroupNode.getNodeName());
-            if (midgroupNode.getNodeType() == Node.ELEMENT_NODE) {
-              Element midgroupElement = (Element) midgroupNode;
-              System.out.print(" name : ");
-              System.out.println(midgroupElement.getAttribute("name"));
-              // subgroup
-              NodeList subgroupList = doc.getElementsByTagName("subgroup");
-              for (int subgroupIndex = 0; subgroupIndex < subgroupList.getLength(); subgroupIndex++) {
-                System.out.println("-------------" + subgroupIndex + "-------------");
-                Node subgroupNode = subgroupList.item(subgroupIndex);
-                System.out.print(subgroupNode.getNodeName());
-                if (subgroupNode.getNodeType() == Node.ELEMENT_NODE) {
-                  Element subgroupElement = (Element) subgroupNode;
-                  System.out.print(" name : ");
-                  System.out.println(subgroupElement.getAttribute("name"));
-                  // entity
-                  NodeList entityList = doc.getElementsByTagName("entity");
-                  for (int entityIndex = 0; entityIndex < entityList.getLength(); entityIndex++) {
-                    Node entityNode = entityList.item(entityIndex);
-                    System.out.print(entityNode.getNodeName());
-                    if (entityNode.getNodeType() == Node.ELEMENT_NODE) {
-                      Element entityElement = (Element) entityNode;
-                      System.out.print(" name : ");
-                      System.out.println(entityElement.getAttribute("name"));
-                      // property
-                      NodeList propertyList = entityElement.getElementsByTagName("property");
-                      for (int propertyIndex = 0; propertyIndex < propertyList.getLength(); propertyIndex++) {
-                        Node property = propertyList.item(propertyIndex);
-                        if (property.getNodeType() == Node.ELEMENT_NODE) {
-                          Element propertyElement = (Element) property;
-                          System.out.print(propertyElement.getAttribute("name") + " : ");
-                          System.out.println(propertyElement.getTextContent());
-                        }
-                      }
-                    }
-                  }
-                }
-              }
-            }
-          }
-        }
-      }
-    } catch (Exception e) {
-      e.printStackTrace();
-    }
   }
 
   public final static String xml = "xml";
@@ -126,4 +51,16 @@ public class XmlUtils {
     return ext;
   }
 
+
+  public static String[] findAllColumns(List<Row> rows) {
+    Set<String> columns = new HashSet<>();
+    for (Row row : rows) {
+      for (Column column : row.getColumns()) {
+        if (!columns.contains(column.getColumn())) {
+          columns.add(column.getColumn());
+        }
+      }
+    }
+    return columns.toArray(new String[columns.size()]);
+  }
 }
